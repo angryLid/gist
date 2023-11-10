6accd91e085cc5c46fcc161c6d7e588890cda07c
angrylid <31571456+angryLid@users.noreply.github.com>
2023-11-10 19:43:20 +0800

    jira functions
    relative date
    filter my rows

--- workEnhancer.user.js
+++ workEnhancer.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Work Enhancer
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @grant        GM_openInTab
+// @grant        GM_addStyle
 // @match        http://qa-yapi.xsl.link/*
 // @match        https://jira.xingshulin.com/*
 // @match        https://lanhuapp.com/*
@@ -98,19 +99,6 @@
     }
 
 
-    function GM_addStyle(css) {
-        const style =
-              document.getElementById("GM_addStyleBy8626") ||
-              (function () {
-                  const style = document.createElement("style");
-                  style.id = "GM_addStyleBy8626";
-                  document.head.appendChild(style);
-                  return style;
-              })();
-        const sheet = style.sheet;
-        sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
-    }
-
     const redirectMapping = {
         ['qa-xhcp.xingshulin.com']() {
             const button = {}
@@ -142,5 +130,127 @@
 
     })
 
+    function calcRelativeDate(){
+        const rows = document.querySelectorAll("issuetable-web-component table tbody tr");
+        const now = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())
+        const t = 86_400_000
+        if(!rows) throw new Error("rows is null")
+
+        Array.from(rows).forEach(ele => {
+            const slashedDate = ele.querySelector(".duedate").innerText
+
+            const [year, month, day] = [
+                slashedDate.slice(0, 4),
+                slashedDate.slice(5, 7),
+                slashedDate.slice(8, 10),
+            ]
+
+            const thisDate = new Date(year, month - 1, day)
+            const dict = "周日，周一，周二，周三，周四，周五，周六".split("，")
+
+            let relativeDay = dict[thisDate.getDay()]
+
+            const gap = (now - thisDate) / t
+
+
+            // 计算当前周的周一和周日
+            const today = now
+            const oneDay = t
+            const input = thisDate
+            const dayOfWeek = today.getDay();
+            const mondayOffset = (dayOfWeek === 0 ? -6 : 1 - dayOfWeek); // 如果今天是周日(dayOfWeek === 0)，则需要向后调整6天
+            const sundayOffset = 7 - dayOfWeek;
+
+            const mondayThisWeek = new Date(today.getTime() + mondayOffset * oneDay);
+            const sundayThisWeek = new Date(today.getTime() + sundayOffset * oneDay);
+
+            // 判断本周
+
+
+            // 计算下周的周一和周日
+            const mondayNextWeek = new Date(sundayThisWeek.getTime() + oneDay);
+            const sundayNextWeek = new Date(mondayNextWeek.getTime() + 6 * oneDay);
+
+            // 判断下周
+
+
+            const mondayNNextWeek = new Date(mondayNextWeek.getTime() + oneDay);
+            const sundayNNextWeek = new Date(sundayNextWeek.getTime() + 6 * oneDay);
+
+            // 判断下周
+            if (input >= mondayThisWeek && input <= sundayThisWeek) {
+                relativeDay = '本' + relativeDay;
+            }else if (input >= mondayNextWeek && input <= sundayNextWeek) {
+                relativeDay = '下' + relativeDay;
+            }else if (input >= mondayNNextWeek && input <= sundayNNextWeek) {
+                relativeDay = '再下' + relativeDay;
+            }
+
+            switch(gap){
+                case 0: {
+                    relativeDay = "今天"
+                    break
+                }
+                case 1: {
+                    relativeDay = "明天"
+                    break
+                }
+                case 2: {
+                    relativeDay = "后天"
+                    break
+                }
+            }
+            ele.querySelector(".duedate")
+                .insertAdjacentHTML("beforeend", `<div>${relativeDay}</div>`)
+        })
+    }
+
+
+    function filterMine() {
+        const username = document.querySelector('meta[name="ajs-remote-user-fullname"]').content
+        const checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        const label = document.createElement("label")
+        label.innerText = "只显示我的"
+        const attach = document.querySelector("#attachmentmodule")
+        const wrapper = document.createElement("div")
+        wrapper.insertAdjacentElement("beforeend", label)
+        wrapper.insertAdjacentElement("beforeend", checkbox)
+
+        attach.insertAdjacentElement("afterend", wrapper)
+
+        const hide = function (e) {
+            e.style.position = 'fixed'
+            e.style.top = '-999px'
+        }
+        const show = function(e) {
+            e.style.position = 'static'
+        }
+        checkbox.onchange = function() {
+            const rows = document.querySelectorAll("issuetable-web-component table tbody tr");
+
+            console.log(this)
+            Array.from(rows).forEach(ele => {
+                const assignee = ele.querySelector(".assignee")
+                if(!assignee.innerText.includes(username)){
+                    if(this.checked) {
+                       hide(ele)
+                    }else {
+                       show(ele)
+                    }
+                }
+            })
+
+        }
+
+    }
+
+    if(location.href.includes("jira.xingshulin.com/browse")) {
+        setTimeout(() => {
+            calcRelativeDate()
+            filterMine()
+        }, 1000)
+
+    }
 
 })();
